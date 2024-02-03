dcd0d3106c32ed08afcd0928771643b0fe0c826c
Serhii Zelinskyi <zelinskyi.serh@gmail.com>
2024-02-03 22:07:53 +0200

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    semi: ["error", "always"],
+    quotes: ["error", "single"],
+    "no-multiple-empty-lines": ["error", { max: 1, maxEOF: 0 }],
+    "eol-last": ["error", "always"],
+    "keyword-spacing": ["error", { before: true }],
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+};
