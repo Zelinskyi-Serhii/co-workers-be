5ce3a8a3ffd713acfd2a8b25b23866956207c0d3
Serhii Zelinskyi <zelinskyi.serh@gmail.com>
2024-04-28 23:19:19 +0300

    added port variable

--- src/main.ts
+++ src/main.ts
@@ -4,19 +4,25 @@ import { AppModule } from './app.module';
 import { getSwaggerDocument } from './swagger';
 import { ValidationPipe } from '@nestjs/common';
 
+const PORT = 5555;
+
 (async () => {
   const app = await NestFactory.create(AppModule);
-  
+
   app.setGlobalPrefix('api/v1');
   app.enableCors({
-    origin: ['http://localhost:3000','http://localhost:3002', 'https://co-workers-fe.vercel.app'],
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3002',
+      'https://co-workers-fe.vercel.app',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   });
   app.useGlobalPipes(new ValidationPipe());
 
   SwaggerModule.setup('api/v1/docs', app, getSwaggerDocument(app));
 
-  await app.listen(5555, () => {
-    console.log('Server is running on http://localhost:5555/api/v1');
+  await app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}/api/v1`);
   });
 })();
